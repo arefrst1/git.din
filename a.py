6c6fbf3f9ae37b0ae195b66ffd5d077bb536c3a5
arefrst1 <arefrst1382@gmail.com>
2025-04-20 13:03:30 +0330

    rrrrr

---
+++ a.py
@@ -0,0 +1,87 @@
+# "aref"=str
+# true,false = bool
+# 16244=int
+# 9.5=float
+
+# print('aref rostami')
+# print(type('aref rostami'))
+
+# print(10-7)
+
+# print(type(100/25))
+
+# a = 26 
+# b =19
+# print( a * b )
+
+# x=int(2.9)
+# print(x)
+
+# c= float(2)
+# print(c)
+
+# name = 'aref\nrostami'
+# print(name)
+
+# a= 'hi_my_name_isaref'
+# print(a[:11])
+
+# a= 'hi_my_name_isaref'
+# print(a[-7:])
+
+# s="aref"
+# b=" rostami"
+# me=s + b
+# print (me)
+
+# age= 21                       age is int and txt is str so 
+# txt="my age is:" +str(age)
+# print(txt)
+
+# age =21       kar bord  (  f and {}  )
+# mooo =f"my age is:{age}"
+# print(mooo)
+
+
+#                  format string
+# Name= "aref"
+# last_name = "rostami"
+# city = "norabad"
+# me =f'my name is {Name} {last_name} and  Mycity is {city}'
+# print (me)
+
+# txt='    AREF rostami       '
+# print(txt.capitalize())
+# print(txt.casefold())
+# print(txt.count('r'))
+# print(txt.find(' '))
+# print(txt.format('21' ,'shiraz'))
+# print(txt.strip()) 
+# print(txt.title())
+# print(txt.upper())
+# print(txt.lower())
+
+# abc=["aref","efo","emad","dady","sina","mmd","hg40"]
+# print(abc[:])
+# a=["aaa","bbb","ccc","ddd","eee"]
+# a[:3]="1"
+# print(a)
+# a=["aaa","bbb","ccc","ddd","eee"]
+# a[0]="1"
+# print(a)
+
+# abc=["aref","efo","emad","dady","sina","mmd","hg40"]
+# abc.append("ak47")
+# print(abc)
+
+# abc=["aref","efo","emad","dady","sina","mmd","hg40"]
+# abc.insert(3,("ak47"))
+# print(abc)
+
+abc=["aref","efo","emad","dady","sina","mmd","hg40"]
+newabc=["aakn","xDAKNQ","zADAA","ADAd"]
+newabc.extend(abc)
+print(newabc)
+
+
+print('sina')
