02b97fc49e0790d4373b0f9bc083bcb04b047098
arefrst1 <arefrst1382@gmail.com>
2025-04-21 01:49:59 +0330

    cnm

--- New folder/function.py
+++ New folder/function.py
@@ -1,4 +1,3 @@
- 
 
 # def abuteme(age , lastname):
 #     print (f"{age} 21 years old")
@@ -27,9 +26,27 @@
 # aj ('aref','rostami','ava', age=21 , team = "ajkj")  
 
 
-def heloo (n):
-    for item in n:
-        print (f"heloo {item}")
+# def heloo (n):
+#     for item in n:
+#         print (f"heloo {item}")
+
+# heloo (["aref","efo","emad"])
 
 
-heloo (["aref","efo","emad"])
+# def abn(a,b):
+#     c = a+b
+#     return c
+# print (abn(10,17))
+
+username = input ("enter your user name : ")
+
+def key (username):
+     if len (username) > 10:
+        return False
+     else:
+         return True 
+     
+if key(username):
+    print ("your use is ok ")
+else:
+    print ("ridi dashi ")
