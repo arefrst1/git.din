ab56c7809c3050c7e3b64dcc3a0cbb3b45f9b15b
arefrst1 <arefrst1382@gmail.com>
2025-04-21 13:51:17 +0330

    ajgs

---
+++ lahflkfklf/booleans.py
@@ -0,0 +1,19 @@
+# print(bool("aref rostami"))
+# print(bool(21))
+
+# print(bool(False))
+# print(bool(None))
+# print(bool(0))
+# print(bool(""))
+
+
+a = 21
+b = 23
+# x = a==b
+# print(x)
+# print(a==b)
+# print(a!=b)
+# print(a > b)
+# print(a < b)
+# print(a >= b)
+# print(a <= b)
